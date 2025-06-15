3d4d941a1cde7d14c34e1df733d14de1dbe2adbb
Akash Prajapati <akashprajapati41488@gmail.com>
2025-06-15 12:06:25 +0530

    axios base url changed

--- src/utils/configureAxios.js
+++ src/utils/configureAxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const configureAxios = () => {
-    axios.defaults.baseURL = process.env.BACKEND_URL;
+    axios.defaults.baseURL = process.env.VITE_API_URL;
     axios.defaults.withCredentials = true;
 };
